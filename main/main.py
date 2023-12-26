9569b700cfa3d5f0c23affc43ecce9258e895423
azval <yousefm2315@gmail.com>
2023-12-25 20:02:57 -0500

    test

--- main/main.py
+++ main/main.py
@@ -1,10 +1,17 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
-class MyApp(App):
+class HelloText(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.add_widget(Label(text="Hello",
+                              font_size=64))
+class SimpleApp(App):
     def build(self):
-        return Label(text="Hello")
+        return HelloText()
 
 if __name__ == "__main__":
-    MyApp().run()
+    SimpleApp().run()
